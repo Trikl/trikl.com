2b3a853724a2f0d914d14d47da78ca4ac6afcdc6
mrbirdman <thebirdman.08@gmail.com>
2013-02-10 17:17:05 -0800

    General cleanup to header and frontage, rearrange menu (half complete)

--- public/views/menu.tpl.php
+++ public/views/menu.tpl.php
@@ -1,47 +1,35 @@
-<div id="left_mouseline">
-	<div id="stuff">
-		<div id="sidebar_left">
-				<?php $user = UserQuery::create()->findPK($_SESSION['uid']); ?>
-			<a href="profile/<?php echo $user->getUsername(); ?>"><img class="usr_img" style="margin-left:8px;float:none;" src="public/avatars/<?php echo $user->getAvatarFilename(); ?>" /></a>
+				<ul id="omnilinks">
+<li id="trikl"><a  href="stream"> Trikl</a></li>
+<li id="photos"><a  href="photo"> Photos</a></li>
+<li id="events"><a  href="events"> Events</a></li>
+<li id="profile"><a  href="profile"> Profile</a></li>
+<li ><a id="settings"> Settings</a></li>
+<li id="logout"><a  href="Logout"> Logout</a></li>
+				</ul>
 
- 			<hr />
-			<div class="menu">
-				<a href="stream">
-					<div class="menu_item" id="stream">
-						<span class="item"><img class="menu_icon" src="public/images/icons/home.png" /> Home</span>
-					</div>
-				</a>
-				<a href="photo">
-					<div class="menu_item" id="photos">
-						<span class="item"><img class="menu_icon" src="public/images/icons/home.png" /> Photo</span>
-					</div>
-				</a>
-				<a href="friends">
-					<div class="menu_item" id="photos">
-						<span class="item"><img class="menu_icon" src="public/images/icons/home.png" /> Friends</span>
-					</div>
-				</a>
-				<a href="messages">
-					<div class="menu_item" id="photos">
-						<span class="item"><img class="menu_icon" src="public/images/icons/home.png" /> Messages</span>
-					</div>
-				</a>
-				<a href="events">
-					<div class="menu_item" id="stream">
-						<span class="item"><img class="menu_icon" src="public/images/icons/home.png" /> Events</span>
-					</div>
-				</a>
-				<!-- <a href="settings"> -->
-					<div class="menu_item" id="settings">
-					    <span class="item"><img class="menu_icon" src="public/images/icons/settings_icon.png" /> Settings</span>
-					</div>
-				<!-- </a> -->
-				<a href="logout">
-					<div class="menu_item">
-					    <span class="item"><img class="menu_icon" src="public/images/icons/logout_icon.png" /> Logout</span>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-</div>
+
+<script>
+								   
+									   $('#settings').click(function() {	
+										$.ajax({
+											type: "POST",
+											url: "/settings",
+											data: {
+												"URL": $(this).attr('url'),
+												"action": "geturl",
+											},
+											success: function(response) {
+												//alert(response);
+												$('#hey').html(response).hide().dialog(function() {});
+												$('#blackout').show();
+												$('.header').stop().animate({ 'height': 45 },1);
+												$('.contents').css("margin-top", "45px");
+												$('body').css("overflow", "hidden");
+											}
+										})
+										$('div#hey').bind('dialogclose', function(event) {
+												$('#blackout').hide();
+												$('body').css("overflow", "scroll");
+										});
+										});
+</script>
