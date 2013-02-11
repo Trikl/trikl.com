05c9278d5458f30fa1aa89020a5ff6702d4d76a6
mrbirdman <thebirdman.08@gmail.com>
2013-02-10 17:17:16 -0800

    General cleanup to header and frontage, rearrange menu (half complete)

--- public/views/frontmenu.tpl.php
+++ public/views/frontmenu.tpl.php
@@ -1,17 +1,15 @@
-<div class="sidebar_left">
-	<a id="login">
-		<div class="menu_item">
-			<span class="item">Login</span>
-		</div>
-	</a>
-	<a id="register">
-		<div class="menu_item">
-		    <span class="item">Register</span>
-		</div>
-	</a>
-	<a href="about">
-		<div class="menu_item" id="about">
-		    <span class="item">About</span>
-		</div>
-	</a>
+<div class="notstream">
+	<img src='/public/images/logo/Small.png' />
+	<div id="menu">
+		<a id="login">
+			<div class="menu_item">
+				<span class="item">Login</span>
+			</div>
+		</a>
+		<a id="registerbtn">
+			<div class="menu_item">
+			    <span class="item">Register</span>
+			</div>
+		</a>
+	</div>
 </div>
